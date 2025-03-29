962324935a1058713b995e5c88ae1f254bba9580
kingaranjoroge <briankingara4@gmail.com>
2025-03-29 21:51:44 +0300

    submit assignment

---
+++ assignment.py
@@ -0,0 +1,25 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent > 20:
+        discount = price * (discount_percent / 100)
+        final_price = price - discount
+        return final_price
+    else:
+        return price
+
+print(calculate_discount(100, 20))
+print(calculate_discount(100, 25))
+
+print("\n")
+
+def calculate_discount(price, discount_percent):
+    if discount_percent > 20:
+        discount = price * (discount_percent / 100)
+        final_price = price - discount
+        return final_price
+    else:
+        return price
+    
+price = int(input("Enter price: "))
+discount_percent = int(input("Enter discount percentage: "))
+    
+print(calculate_discount(price, discount_percent))
